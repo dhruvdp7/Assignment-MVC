48ed76c06bbe4ec493ba58cac1282792eb90e254
dhruvdp7 <46739476+dhruvdp7@users.noreply.github.com>
2019-01-31 16:02:58 +0530

    Addition logic removed
    The addition logic is been removed and added to the controller.

--- Calculator/src/CalculatorModel.java
+++ Calculator/src/CalculatorModel.java
@@ -2,10 +2,10 @@ public class CalculatorModel{
 
 	public int result;
 	
-	public void add(int num1, int num2){
-
-		result=num1+num2;
-
+	public void setResult(int result) {
+		
+	this.result= result;	
+		
 	}
 
 	public int getResult(){
